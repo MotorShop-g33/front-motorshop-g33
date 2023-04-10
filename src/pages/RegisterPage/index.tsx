445d39a07654adf4c8f74224dab011bf41db520c
Marcelo Henrique Aguiar Marques <96503481+MHAMarques@users.noreply.github.com>
2023-04-10 16:17:30 -0300

    Feat: Dashboard Structure
    Estrutura inicial da home page com estilização

--- src/pages/RegisterPage/index.tsx
+++ src/pages/RegisterPage/index.tsx
@@ -1,7 +1,11 @@
+import { Link } from "react-router-dom";
+
 export const RegisterPage = () => {
   return (
     <>
       <h1>Eu sou a RegisterPage</h1>
+      <Link to={"/"}>Home page</Link><br />
+      <Link to={"/login"}>Login</Link>
     </>
   );
 };
