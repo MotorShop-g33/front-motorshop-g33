d655549b99c53dd0026cdf532c15c0aa0523e23d
Daniel Lobato <daniel.lobato2001@gmail.com>
2023-04-20 16:29:43 -0300

    fix: corrects words

--- src/validateSchemas/validateAnnouncementSchema.ts
+++ src/validateSchemas/validateAnnouncementSchema.ts
@@ -1,15 +1,15 @@
 import * as yup from "yup";
 
 export const CreateAnnouncementSchema = yup.object().shape({
-  brand: yup.string().required("Marca obrigatório"),
+  brand: yup.string().required("Marca obrigatória"),
   model: yup.string().required("Modelo obrigatório"),
   year: yup.number().required("Ano obrigatório"),
-  fuel: yup.string().required("Combustível obrigatorio"),
-  milage: yup.string().required("Quilometragem obrigatorio"),
-  color: yup.string().required("Cor obrigatoria"),
+  fuel: yup.string().required("Combustível obrigatório"),
+  milage: yup.string().required("Quilometragem obrigatória"),
+  color: yup.string().required("Cor obrigatória"),
   fipe: yup.number().notRequired(),
-  price: yup.number().required("Preço obrigatorio"),
-  description: yup.string().required("Descrição obrigatoria"),
-  avatar: yup.string().required("Imagem da capa obrigatorio"),
+  price: yup.number().required("Preço obrigatório"),
+  description: yup.string().required("Descrição obrigatória"),
+  avatar: yup.string().required("Imagem da capa obrigatória"),
   photos: yup.array().of(yup.string().url().notRequired()),
 });
