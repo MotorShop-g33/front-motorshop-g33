a8191877920e49007819d8216ae029ca4656c36a
Daniel Lobato <daniel.lobato2001@gmail.com>
2023-05-08 13:44:08 -0300

    fix: timeout axios and defaultvalue edit announ

--- src/services/index.ts
+++ src/services/index.ts
@@ -4,7 +4,7 @@ const token: string | null = localStorage.getItem("@token:token");
 
 export const api = axios.create({
   baseURL: "http://localhost:3000/",
-  timeout: 3000,
+  timeout: 6000,
   headers: {
     "Content-Type": "application/json",
     Authorization: `Bearer ${token}`,
